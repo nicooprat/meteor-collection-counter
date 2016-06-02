c5fbe5057343eb14765d453a12e92639a1025b92
Nico Prat <nicooprat@gmail.com>
2016-06-02 18:42:21 +0200

    Improvement on calculating updated fields values
    Instead of calculating all possible operations, create a temporary local document using Minimongo, apply the modifier, and get the modified document. Too bad I didn't find this a few days earlier...
    
    https://github.com/matb33/meteor-collection-hooks/issues/91

--- counter.js
+++ counter.js
@@ -1,5 +1,21 @@
 c2 = Package['aldeed:collection2'];
 
+simulateDocumentModifier = function(doc, modifier) {
+  var c = new Package.minimongo.LocalCollection('simulations');
+  c.insert(doc);
+
+  var sel = {
+    _id: doc._id
+  };
+
+  c.update(sel, modifier);
+
+  var sim = c.findOne(sel);
+  c.remove(sel);
+
+  return sim;
+}
+
 CollectionBehaviours.define('counter', function(opts = {}) {
 
   // Options
@@ -33,53 +49,20 @@ CollectionBehaviours.define('counter', function(opts = {}) {
     });
   });
 
-  // We must execute before update to avoid extra queries after doc is inserted
   this.collection.before.update((userId, doc, fieldNames, modifier, opts) => {
-    // We must calculate the future length of the Array source field
-    // Before Mongo operates, for each possible operator
-    // /!\ Doesn't support the `$slice` operator /!\
-    // https://docs.mongodb.com/manual/reference/operator/update-array/
-    _.each(options, (field) => {
-      // $push -> $inc + 1
-      if( modifier.$push && modifier.$push[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = 1;
-      }
-
-      // $pushAll -> $inc + $pushAll.length
-      if( modifier.$pushAll && modifier.$pushAll[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = modifier.$pushAll[field.source].length
-      }
-
-      // $pull -> $inc - 1
-      if( modifier.$pull && modifier.$pull[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = -1;
-      }
-
-      // $pullAll -> $inc - $pullAll.length
-      if( modifier.$pullAll && modifier.$pullAll[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = modifier.$pullAll[field.source].length;
-      }
-
-      // $pull -> $inc - 1
-      if( modifier.$pull && modifier.$pull[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = -1;
-      }
-
-      // $set -> $set
-      if( modifier.$set && modifier.$set[field.source] ){
-        modifier.$set[field.destination] = modifier.$set[field.source].length
-      }
-
-      // $addtoset -> $inc + $addtoset.length
-      if( modifier.$addtoset && modifier.$addtoset[field.source] ){
-        modifier.$inc = modifier.$inc || {};
-        modifier.$inc[field.destination] = _.union(modifier.$addtoset[field.source], doc[field.source]).length;
-      }
+    // Get only fields updated & watched
+    const fields = _(options).filter((option) => {
+      return _(fieldNames).contains(option.source);
     });
+
+    if( !_(fields).isEmpty() ) {
+      // Simulate the document after update
+      const simulation = simulateDocumentModifier(doc, modifier);
+
+      _.each(fields, (field) => {
+        modifier.$set = modifier.$set || {};
+        modifier.$set[field.destination] = simulation[field.source].length;
+      });
+    }
   });
 });
